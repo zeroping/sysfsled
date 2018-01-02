5ccd5dd1e56cea07ffb60fa968ce5c15976fa322
Scott <scott.gilliland@gatech.edu>
2018-01-02 17:55:31 -0500

    Added brightness reading to example

--- example/src/led_flash.cpp
+++ example/src/led_flash.cpp
@@ -1,5 +1,6 @@
 #include <stdexcept>
 #include <iostream>
+#include <iomanip>
 #include <memory>
 #include <udevpp.h>
 
@@ -11,6 +12,7 @@
 int main(int argc, char **argv)
 {
 	std::cout << "Sysfsled Example" << std::endl;
+	std::cout << std::endl;
 	
 	std::string path = "/sys/devices/platform/thinkpad_acpi/leds/tpacpi::power";
 	if (argc == 2)
@@ -20,9 +22,17 @@ int main(int argc, char **argv)
 	}
 	else
 	{
+		std::cout << "Usage:" << std::endl;
+		std::cout << "  to list LEDs: led_flash" << std::endl;
+		std::cout << "  to blink an LED: led_flash [sysfs path of led]" << std::endl;
+		
 		for (sysfsled::LED led : sysfsled::LED::getSystemLEDs())
 		{
-			std::cout << "Available LED " << led.get_name() << " at sysfs path: " << led.udevice_.get_syspath() << std::endl;
+			std::cout << std::endl;
+			std::cout << "Available LED " << led.get_name() << std::endl;
+			std::cout << "  at sysfs path: " << led.udevice_.get_syspath() << std::endl;
+			std::cout << "  supports " << led.get_brightness_levels() << " levels of brightness" << std::endl;
+			std::cout << "  currently at " << std::fixed << std::setprecision(0) << led.get_brightness()*100 << "% brightness" << std::endl;
 		}
 		return -1;
 	}
